0ffc7b51fd1ba493b2deec708c4e999af640e091
OFreshman <33643257+OFreshman@users.noreply.github.com>
2024-02-18 15:49:46 +0800

    update

--- js/injectJS.js
+++ js/injectJS.js
@@ -5,32 +5,51 @@ const randomTime = new Date().getTime();
 // 添加 env.js
 const js1 = document.createElement("script");
 // js1.src = `./js/es01.js?v=${randomTime}`;
-js1.src = `https://gitee.com/Yqt_18/Learn/blob/master/Test/js/es01.js?v=${randomTime}`;
-headDom.appendChild(js1);
+js1.src = `https://ofreshman.github.io/demoJS/js/es01.js`;
+// headDom.appendChild(js1);
 
 // 添加 request.js
 const js2 = document.createElement("script");
 // js2.src = `./js/es02.js?v=${randomTime}`;
-js2.src = `https://gitee.com/Yqt_18/Learn/blob/master/Test/js/es02.js?v=${randomTime}`;
+js2.src = `https://ofreshman.github.io/demoJS/js/es02.js`;
 
-headDom.appendChild(js2);
+// headDom.appendChild(js2);
 
 
 // 添加 api.js
 const js3 = document.createElement("script");
 // js3.src = `./js/es03.js?v=${randomTime}`;
-js3.src = `https://gitee.com/Yqt_18/Learn/blob/master/Test/js/es03.js?v=${randomTime}`;
-headDom.appendChild(js3);
+js3.src = `https://ofreshman.github.io/demoJS/js/es03.js`;
+// headDom.appendChild(js3);
 
+injectJSOrderly([js1, js2, js3])
 
 window.onload = function () {
   const bodyDom = document.getElementsByTagName("body")[0];
   // 添加 index.js
   const js4 = document.createElement("script");
   // js4.src = `./js/es02.js?v=${randomTime}`;
-  js4.src = `https://gitee.com/Yqt_18/Learn/blob/master/Test/js/index.js?v=${randomTime}`;
+  js4.src = `https://ofreshman.github.io/demoJS/js/index.js`;
   bodyDom.appendChild(js4);
 };
+
+/**
+ * 动态注入JS，并确保有序
+ * @param pathArr js 路径数组
+ */ 
+function injectJSOrderly(jsArr = []) {
+  const resolveFn = null;
+  let p = Promise.resolve();
+  jsArr.forEach(jsEle => {
+    p = p.then(() => new Promise(resolve => {
+      headDom.appendChild(jsEle)
+      jsEle.onload = (evt, e) => {
+        console.log("onload: ", evt, e);
+        resolve();
+      }
+    }))
+  })
+}
 
 
 /**
